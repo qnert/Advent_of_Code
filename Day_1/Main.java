fcc528c03e5ac9cf1ebc8c2962cb33539130192d
qnert <qnertsimon@gmail.com>
2024-12-01 18:37:57 +0100

    added the part two

--- Day_1/Main.java
+++ Day_1/Main.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main{
+    public static int calc_similarity(int[] arr, int nb){
+        int counter = 0;
+        for (int i: arr){
+            if (i == nb)
+                counter++;
+        }
+        return counter;
+    }
+
     public static int calc_distance(int[] left, int[] right){
         int sum = 0;
         Arrays.sort(left);
@@ -17,6 +26,7 @@ public class Main{
     public static void main(String[] args){
         try{
             long sum = 0;
+            long apperance = 0;
             int[] left = new int[1000];
             int[] right = new int[1000];
             File myObj = new File(args[0]);
@@ -27,8 +37,15 @@ public class Main{
                 left[i] = Integer.parseInt(line[0]);
                 right[i++] = Integer.parseInt(line[1]);
             }
+            //part 1
             sum = calc_distance(left, right);
+
+            //part 2
+            for (int j = 0; j < 1000; j++){
+                apperance += left[j] * calc_similarity(right, left[j]);
+            }
             System.out.println("The total distance is: " + sum);
+            System.out.println("The similarity factor is: " + apperance);
         }
         catch(Exception e){
             System.out.println("Error!");
